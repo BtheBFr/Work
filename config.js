7cfacbe571ebfffcb957cfe1071fd5ee9b702409
BtheBFr <rtfmqwertu29v@gmail.com>
2026-02-27 17:29:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,5 @@
-// ⚠️ КОНФИГУРАЦИЯ ДЛЯ WORK ОТ "B THE B"
-
+// Конфигурация для сайта
 const CONFIG = {
-    // ID Google таблицы
-    spreadsheetId: '1KvGjGBkjUPMFdI2KPsa07wWie31FGEKZpUfbPjEters',
-    
-    // API ключ (только что созданный)
-    apiKey: 'AIzaSyCWweYCE6tloi18_Cq-zZwX7Z876k-Yuqc',
-    
-    // ID папки для чеков
-    driveFolderId: '1LqsHTHORwj69EmbTwumCz0prFm94Kd3v',
-    
-    // Данные Service Account (для Drive API)
-    serviceAccount: {
-        client_email: "work-btheb-uploader@work-btheb.iam.gserviceaccount.com",
-        private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC8b4aadSC+ft25\nP78pSm/Apn6mO7WaWDjBSs3ZSmGqmnv9nga6uzAAM9DDj8izfbqHtKX97xqyqtrU\njb/mDUst7rATcx7o1tu4B+Z0JanKTT3jDi4n4bePpXOjQRTHry9HoYw1Hpo++CEJ\nXd1nw+cjPbBRyLwwtz0VYAjp4osAHkS7H/mFrHldYU9aV4b5iaarCdqECMJzXgL8\nzeAFwCNHsD/jdUL+c6uwuioaSzfKhMH95kr2NBELtM5kiono0a7Cn6U/y+NMvsWF\nCBmS7F2C2S13j6Uij0ZbOpv7O9AETZ2PlkS/us574JKdSP328Gr1yoDOAxNOl7AC\nuE2OlwjTAgMBAAECggEAAXpWceutQBE6p/NKCobc7YPgxmj9eaJrf4iYqLWeonnn\nswuEPf6t17KQVphQGKhJyDqwepdffDs+0B/0e1drWYbftqdWfLkCrCTTt+F7vWxx\nqdA4qHNqvyOrDoK6KP7EvikUMm51Az1q9OnZjLDKpUockeejIjatsfc42qy+LlgK\n9nyrN3TeVa6ZIzK9FNMbK3IO/Eb6N1gG4jQCWQc51+q6fNCAbnGMtfEDv3+pkIwv\nMA/wh4TmVP8CbVOHuUIhOPpq6ebwI2kwoPIEu5qtrhdoVLf1T8sX+rpCHYJ7fXja\nWw74O0GM5o3a1lX4R34slwYXNwYcxeJMf8VCBDVgwQKBgQD1kI7Q4BxmTxZtG4Pv\n6+NPGpkLy81gUbuEdx3ALn0D+9Zdg8WJnSqDzyb3oD3ps8CkGZpq1t4DaAfjO8cl\nmcqB9hUUCZVaqN/WhjTvtZfOuGzF2BPW0NlTaohCviNdba45tFn3aSIj6jjXzrSb\nD/o1f8vtsgitVBlgZzT9Ge2SMQKBgQDEcXlmV9Lq7/HSaLmNT0yx73STyDAuLHiL\nW2ghUP/y5faR6qYjfi6um/vxeVl50CrZCZZJ+sOzC5lIoFFP/mqL36WEGmVUqy2b\nQT3oTy4Q5cXYHtdu0QLDN01SYadjq166j94GgIqkik5iDuy70tBQwVwXCWMXSaX2\n61KcbP6mQwKBgQCy/RgsBX6N0A2/n3OuuBhqOy/c2kMrdsegiRZFU11KPxED/p1I\nbidkBeUYT1NxedTrh8H3KACRKQ2+wn6o2I0z50N75HRZrYfiHNaO9qTdR635E0N6\nq6YM2DTTuTWbzutUIjIFVTtORFYEbyVTdPTic/EXo1IRzJ/mlbOY3II+sQKBgBpV\n5CKWT45zkIXWyFlrcirAoW5Wfi8Sgy6bmIAu7cFQlxfEz3Ueq395JfOfgyFTiQsD\nwAQ2/Bl6ypfz9jK1Nu99kiD9sOwC7uBLhNMqGlDlnPp2WAdhOe+V8GXE9b3k8ZQt\nSWSxwuSALjXzLkWX45mP8pqcBY7PrtJFhhO2gQUlAoGBAJX/84B0SGmINeGduRc5\n96U/9EUIelItqShT+l4QFziTwGifRAcc2a/WO7rpAvCae6t4lVX//E02v7QL47dv\nxSRSlKH8pEfzgKKGpIUJu672ZqdHs38bKq9+c4dVmXWk4vaOmo3W4dx0pCEPRCfs\nzq34fq/MjZms3bti74Y/Edfg\n-----END PRIVATE KEY-----\n"
-    }
+    // API URL (твой развернутый скрипт)
+    apiUrl: 'https://script.google.com/macros/s/AKfycbz02h0AraZ90QM6VjxixRZbLvAA-4ZFK1CdqU2BU1zRJJZKhkTfUx_za4PNeNM-02k/exec'
 };
