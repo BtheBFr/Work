4b301a9d034126ec61c9e33263b31a1c4b3bba1d
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-14 23:51:50 +0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Конфигурация для сайта
-const CONFIG = {
-    // API URL (твой развернутый скрипт)
-    apiUrl: 'https://script.google.com/macros/s/AKfycbz02h0AraZ90QM6VjxixRZbLvAA-4ZFK1CdqU2BU1zRJJZKhkTfUx_za4PNeNM-02k/exec'
-};
